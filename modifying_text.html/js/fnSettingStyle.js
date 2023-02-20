057449d49c1024858b23861c2cc9dee94f557616
jemitchppu <122327944+jemitchppu@users.noreply.github.com>
2023-02-19 20:49:22 -0500

    Add files via upload

---
+++ modifying_text.html/js/fnSettingStyle.js
@@ -0,0 +1,25 @@
+function enlargeText() {
+    document.getElementById('p1').style.fontSize = "5em";
+    document.getElementById('p2').style.fontSize = "5em";
+    document.getElementById('p3').style.fontSize = "5em";
+    document.getElementById('p4').style.fontSize = "5em";
+    document.getElementById('p5').style.fontSize = "5em";
+}
+
+function decreaseText() {
+    document.getElementById('p1').style.fontSize = "1em";
+    document.getElementById('p2').style.fontSize = "1em";
+    document.getElementById('p3').style.fontSize = "1em";
+    document.getElementById('p4').style.fontSize = "1em";
+    document.getElementById('p5').style.fontSize = "1em";
+}
+
+function enlargeHead() {
+    document.getElementById('head1').style.fontSize = "7em";
+    document.getElementById('head2').style.fontSize = "7em";
+}
+
+function decreaseHead() {
+    document.getElementById('head1').style.fontSize = "1em";
+    document.getElementById('head2').style.fontSize = "1em";
+}
