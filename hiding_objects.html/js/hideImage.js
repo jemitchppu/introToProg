9fd12affef8c0cd3e538e3e9760b2e49e7839d45
jemitchppu <122327944+jemitchppu@users.noreply.github.com>
2023-02-19 20:31:53 -0500

    Module 6 Hiding Objects

---
+++ hiding_objects.html/js/hideImage.js
@@ -0,0 +1,23 @@
+function hide1() {
+    document.getElementById('cover1').className = "hidden";
+}
+
+function hide2() {
+    document.getElementById('cover2').className = "hidden";
+}
+
+function hide3() {
+    document.getElementById('cover3').className = "hidden";
+}
+
+function hide4() {
+    document.getElementById('cover4').className = "hidden";
+}
+
+function hide5() {
+    document.getElementById('cover5').className = "hidden";
+}
+
+function hide6() {
+    document.getElementById('cover6').className = "hidden";
+}
